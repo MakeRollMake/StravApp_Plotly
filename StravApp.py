37a8a1902877ee9184711771873336fe7f2721ad
MakeRollMake <50707253+MakeRollMake@users.noreply.github.com>
2023-02-06 19:49:35 +0100

    Host 0000

--- StravApp.py
+++ StravApp.py
@@ -362,5 +362,5 @@ app.layout = html.Div([
 
 # Run app
 if __name__ == '__main__':
-    app.run_server(debug=True)
-    # app.run_server(host='0.0.0.0', debug=True)
+    #app.run_server(debug=True)
+    app.run_server(host='0.0.0.0', debug=True)
